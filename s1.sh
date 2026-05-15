5510114fb0c81087929f5fcc3bc27c70e76dcbc9
James G2 <jagodoyra@uide.edu.ec>
2026-05-14 20:33:23 -0500

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,5 +1,5 @@
-uname -r
 # Me muestra la nube sobre la que corre esa VM
-# Me muestra toda la informacion de mi sistema operativo y el hardware as
+uname -r
 
+ %Me muestra toda la informacion de mi sistema operativo y el hardware as
 uname -a
