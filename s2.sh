1503de4c884957edc58c2fdeab91006ee12af40a
James G2 <jagodoyra@uide.edu.ec>
2026-05-20 01:52:49 +0000

    wip: basic commands

--- s2.sh
+++ s2.sh
@@ -22,3 +22,21 @@ cd
 #ruta relativa: no tiene toda la ruta completa 
 #lista archivos y directorios en el directorio actual
 ls
+#ls: lista archivos y directorios en el directorio actual y 
+#la opcion -a es all de muestrame todo  
+#(incluyendo archivos y directorios ocultos)
+ls -a
+# cambio de directorio al directorio raiz de linux
+cd /
+# cambio de directorio al directorio de binario de linux
+# ruta absoluta
+cd /bin
+#ruta relativa
+cd bin
+#Este comando sirve para instalar tu programa en el sistema para 
+#que pueda ejecutarlo desde cualquier lugar de la terminal simplemente 
+#escribiendo hola_binario.
+sudo cp /workspaces/intro-unix-may-2025-online/hola_binario /bin/hola_binario
+#(Make Directory): Es el comando nativo de Unix/Linux que le ordena al sistema operativo "crear un directorio"
+#nuevo_directorio: Es el nombre que va a tener la carpeta que estás creando.
+mkdir nuevo_directorio
