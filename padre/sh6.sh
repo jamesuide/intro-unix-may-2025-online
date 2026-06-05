9d6d9e25907af5cd712a48ec4d1404eca381b5d6
James G2 <jagodoyra@uide.edu.ec>
2026-06-05 02:05:06 +0000

    SUDO APT

---
+++ padre/sh6.sh
@@ -0,0 +1,6 @@
+#Sirve para actualizar la lista de paquetes disponibles en los repositorios.
+sudo apt update
+#Sirve para instalar las actualizaciones de los programas que ya están instalados.
+sudo apt upgrate 
+#instala la herramienta Nmap, utilizada para analizar redes, identificar equipos conectados y verificar puertos abiertos.
+sudo apt install nmap
---
+++ suma.y
@@ -0,0 +1,7 @@
+def sumar(a, b):
+    return a + b
+
+# Probando la función con dos números
+resultado = sumar(8, 7)
+print("El resultado de la suma es:", resultado)
+
